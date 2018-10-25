2df972123fb1f59d6095d0159df684274488bc22
Mikael Norlén <mickenorlen@gmail.com>
2018-10-25 17:34:34 +0200

    Add to eslintignore

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -14,106 +14,106 @@ console.info('webpack env:', process.env.NODE_ENV);
 
 // PostCss loader conf
 const postCssLoader = {
-    loader: 'postcss-loader',
-    options: {
-        ident: 'postcss',
-        plugins: loader => [
-            postCssPresetEnv({
-                browsers: 'last 4 versions',
-            }),
-            postCssImport({ root: loader.resourcePath }),
-            autoPrefixer,
-            ...(inDev ? [] : [cssNano]),
-        ],
-    },
+  loader: 'postcss-loader',
+  options: {
+    ident: 'postcss',
+    plugins: loader => [
+      postCssPresetEnv({
+        browsers: 'last 4 versions',
+      }),
+      postCssImport({ root: loader.resourcePath }),
+      autoPrefixer,
+      ...(inDev ? [] : [cssNano]),
+    ],
+  },
 };
 
 /* WEBPACK CONF */
 // Production conf
 const webpackConf = {
-    entry: './public/js/index.js',
-    output: {
-        path: path.resolve(__dirname, 'public/build'),
-        filename: 'main.js',
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'main.css',
-        }),
-        new VueLoaderPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    postCssLoader,
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.vue$/,
-                use: {
-                    loader: 'vue-loader',
-                    options: {
-                        loaders: {
-                            js: 'babel-loader',
-                        },
-                    },
-                },
-            },
-            // {
-            //     test: /\.css$/,
-            //     use: ['vue-style-loader', 'css-loader', 'postcss-loader'],
-            // },
+  entry: './public/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public/build'),
+    filename: 'main.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+    new VueLoaderPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          postCssLoader,
+          'sass-loader',
         ],
-    },
-    optimization: {
-        minimizer: [new UglifyJsPlugin()],
-    },
-    // resolve: {
-    //     alias: {
-    //         vue: 'vue/dist/vue.js',
-    //     },
-    // },
+      },
+      {
+        test: /\.vue$/,
+        use: {
+          loader: 'vue-loader',
+          options: {
+            loaders: {
+              js: 'babel-loader',
+            },
+          },
+        },
+      },
+      // {
+      //     test: /\.css$/,
+      //     use: ['vue-style-loader', 'css-loader', 'postcss-loader'],
+      // },
+    ],
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
+  // resolve: {
+  //     alias: {
+  //         vue: 'vue/dist/vue.js',
+  //     },
+  // },
 };
 
 // Development overrides
 if (inDev) {
-    webpackConf.devtool = 'source-maps';
-    postCssLoader.options.sourceMap = true;
-    delete webpackConf.optimization;
+  webpackConf.devtool = 'source-maps';
+  postCssLoader.options.sourceMap = true;
+  delete webpackConf.optimization;
 
-    // Override sass rule conf
-    webpackConf.module.rules[1] = {
-        test: /\.scss$/,
-        use: [
-            'style-loader',
-            {
-                loader: 'css-loader',
-                options: {
-                    sourceMap: true,
-                },
-            },
-            postCssLoader,
-            {
-                loader: 'sass-loader',
-                options: {
-                    sourceMap: true,
-                },
-            },
-        ],
-    };
+  // Override sass rule conf
+  webpackConf.module.rules[1] = {
+    test: /\.scss$/,
+    use: [
+      'style-loader',
+      {
+        loader: 'css-loader',
+        options: {
+          sourceMap: true,
+        },
+      },
+      postCssLoader,
+      {
+        loader: 'sass-loader',
+        options: {
+          sourceMap: true,
+        },
+      },
+    ],
+  };
 }
 
 export default webpackConf;
